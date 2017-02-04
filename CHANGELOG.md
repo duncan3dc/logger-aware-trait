fe13e550a5ddcd11085e98d37637adc004db7c40
Craig Duncan <git@duncanc.co.uk>
2017-02-04 21:17:17 +0000

    Create the LoggerAwareTrait

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,3 +4,9 @@ Changelog
 ## x.y.z - UNRELEASED
 
 --------
+
+## 1.0.0 - 2017-02-04
+
+### Added
+
+* [LoggerAwareTrait] Created the LoggerAwareTrait.
---
+++ tests/LoggerAwareTraitTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace duncan3dc\LogTests;
+
+use duncan3dc\Log\LoggerAwareTrait;
+use PHPUnit\Framework\TestCase;
+use Psr\Log\AbstractLogger;
+use Psr\Log\NullLogger;
+
+class LoggerAwareTraitTest extends TestCase
+{
+    private $class;
+
+    public function setUp()
+    {
+        $this->class = new Class {
+            use LoggerAwareTrait;
+        };
+    }
+
+
+    public function testGetNullLogger()
+    {
+        $logger = $this->class->getLogger();
+        $this->assertInstanceOf(NullLogger::class, $logger);
+    }
+
+
+    public function testSetLogger()
+    {
+        $logger = new Class extends AbstractLogger {
+            use LoggerAwareTrait;
+            public function log($level, $message, array $context = []) {}
+        };
+
+        $this->class->setLogger($logger);
+
+        $this->assertSame($logger, $this->class->getLogger());
+    }
+
+
+    public function testChaining()
+    {
+        $result = $this->class->setLogger(new NullLogger);
+        $this->assertSame($this->class, $result);
+    }
+}
